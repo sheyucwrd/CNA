98469107fbc009a7d6ae4d78543b3f6f2d4466a5
sheyucwrd <157973035+sheyucwrd@users.noreply.github.com>
2025-05-04 22:37:45 +0930

    Add files via upload
     edit #define BIDIRECTIONAL 0

--- sr.h
+++ sr.h
@@ -8,6 +8,7 @@ extern void A_input(struct pkt);
 extern void B_input(struct pkt);
 extern void A_output(struct msg);
 extern void A_timerinterrupt(void);
+#define BIDIRECTIONAL 0
 
 /* optional */
 extern void B_output(struct msg);
